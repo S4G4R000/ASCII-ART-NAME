909573c215585857de86fae3f6665cf579c3ebfd
RAGASx000 <134832705+S4G4R000@users.noreply.github.com>
2023-10-21 20:14:26 -0400

    name.py

--- name.py
+++ name.py
@@ -2,27 +2,58 @@ logo = """
 
 \033[1;35m
   /***
- *      ______    ______    ______    ______   _______  
- *     /      \  /      \  /      \  /      \ |       \ 
- *    |  $$$$$$\|  $$$$$$\|  $$$$$$\|  $$$$$$\| $$$$$$$\
- *    | $$___\$$| $$__| $$| $$ __\$$| $$__| $$| $$__| $$
- *     \$$    \ | $$    $$| $$|    \| $$    $$| $$    $$
- *     _\$$$$$$\| $$$$$$$$| $$ \$$$$| $$$$$$$$| $$$$$$$\
- *    |  \__| $$| $$  | $$| $$__| $$| $$  | $$| $$  | $$
- *     \$$    $$| $$  | $$ \$$    $$| $$  | $$| $$  | $$
- *      \$$$$$$  \$$   \$$  \$$$$$$  \$$   \$$ \$$   \$$
- *     __    __  ________  _______    ______   __       
- *    |  \  |  \|        \|       \  /      \ |  \      
- *    | $$\ | $$| $$$$$$$$| $$$$$$$\|  $$$$$$\| $$      
- *    | $$$\| $$| $$__    | $$__/ $$| $$__| $$| $$      
- *    | $$$$\ $$| $$  \   | $$    $$| $$    $$| $$      
- *    | $$\$$ $$| $$$$$   | $$$$$$$ | $$$$$$$$| $$      
- *    | $$ \$$$$| $$_____ | $$      | $$  | $$| $$_____ 
- *    | $$  \$$$| $$     \| $$      | $$  | $$| $$     \
- *     \$$   \$$ \$$$$$$$$ \$$       \$$   \$$ \$$$$$$$$
- *                                                      
- *                                                      
- *                                                      
+ 
+                                                                                                                       
+                                                                                                                       
+   SSSSSSSSSSSSSSS              AAA                  GGGGGGGGGGGGG               AAA               RRRRRRRRRRRRRRRRR   
+ SS:::::::::::::::S            A:::A              GGG::::::::::::G              A:::A              R::::::::::::::::R  
+S:::::SSSSSS::::::S           A:::::A           GG:::::::::::::::G             A:::::A             R::::::RRRRRR:::::R 
+S:::::S     SSSSSSS          A:::::::A         G:::::GGGGGGGG::::G            A:::::::A            RR:::::R     R:::::R
+S:::::S                     A:::::::::A       G:::::G       GGGGGG           A:::::::::A             R::::R     R:::::R
+S:::::S                    A:::::A:::::A     G:::::G                        A:::::A:::::A            R::::R     R:::::R
+ S::::SSSS                A:::::A A:::::A    G:::::G                       A:::::A A:::::A           R::::RRRRRR:::::R 
+  SS::::::SSSSS          A:::::A   A:::::A   G:::::G    GGGGGGGGGG        A:::::A   A:::::A          R:::::::::::::RR  
+    SSS::::::::SS       A:::::A     A:::::A  G:::::G    G::::::::G       A:::::A     A:::::A         R::::RRRRRR:::::R 
+       SSSSSS::::S     A:::::AAAAAAAAA:::::A G:::::G    GGGGG::::G      A:::::AAAAAAAAA:::::A        R::::R     R:::::R
+            S:::::S   A:::::::::::::::::::::AG:::::G        G::::G     A:::::::::::::::::::::A       R::::R     R:::::R
+            S:::::S  A:::::AAAAAAAAAAAAA:::::AG:::::G       G::::G    A:::::AAAAAAAAAAAAA:::::A      R::::R     R:::::R
+SSSSSSS     S:::::S A:::::A             A:::::AG:::::GGGGGGGG::::G   A:::::A             A:::::A   RR:::::R     R:::::R
+S::::::SSSSSS:::::SA:::::A               A:::::AGG:::::::::::::::G  A:::::A               A:::::A  R::::::R     R:::::R
+S:::::::::::::::SSA:::::A                 A:::::A GGG::::::GGG:::G A:::::A                 A:::::A R::::::R     R:::::R
+ SSSSSSSSSSSSSSS AAAAAAA                   AAAAAAA   GGGGGG   GGGGAAAAAAA                   AAAAAAARRRRRRRR     RRRRRRR
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+NNNNNNNN        NNNNNNNNEEEEEEEEEEEEEEEEEEEEEEPPPPPPPPPPPPPPPPP        AAA               LLLLLLLLLLL                   
+N:::::::N       N::::::NE::::::::::::::::::::EP::::::::::::::::P      A:::A              L:::::::::L                   
+N::::::::N      N::::::NE::::::::::::::::::::EP::::::PPPPPP:::::P    A:::::A             L:::::::::L                   
+N:::::::::N     N::::::NEE::::::EEEEEEEEE::::EPP:::::P     P:::::P  A:::::::A            LL:::::::LL                   
+N::::::::::N    N::::::N  E:::::E       EEEEEE  P::::P     P:::::P A:::::::::A             L:::::L                     
+N:::::::::::N   N::::::N  E:::::E               P::::P     P:::::PA:::::A:::::A            L:::::L                     
+N:::::::N::::N  N::::::N  E::::::EEEEEEEEEE     P::::PPPPPP:::::PA:::::A A:::::A           L:::::L                     
+N::::::N N::::N N::::::N  E:::::::::::::::E     P:::::::::::::PPA:::::A   A:::::A          L:::::L                     
+N::::::N  N::::N:::::::N  E:::::::::::::::E     P::::PPPPPPPPP A:::::A     A:::::A         L:::::L                     
+N::::::N   N:::::::::::N  E::::::EEEEEEEEEE     P::::P        A:::::AAAAAAAAA:::::A        L:::::L                     
+N::::::N    N::::::::::N  E:::::E               P::::P       A:::::::::::::::::::::A       L:::::L                     
+N::::::N     N:::::::::N  E:::::E       EEEEEE  P::::P      A:::::AAAAAAAAAAAAA:::::A      L:::::L         LLLLLL      
+N::::::N      N::::::::NEE::::::EEEEEEEE:::::EPP::::::PP   A:::::A             A:::::A   LL:::::::LLLLLLLLL:::::L      
+N::::::N       N:::::::NE::::::::::::::::::::EP::::::::P  A:::::A               A:::::A  L::::::::::::::::::::::L      
+N::::::N        N::::::NE::::::::::::::::::::EP::::::::P A:::::A                 A:::::A L::::::::::::::::::::::L      
+NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEEPPPPPPPPPPAAAAAAA                   AAAAAAALLLLLLLLLLLLLLLLLLLLLLLL      
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                  
  */
 
 \033[1;31m
